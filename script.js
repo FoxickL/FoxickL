2ce1ef7e932d5fcd4632d481f1f8fc68cdf2dd5c
FoxickL <sadta34@gmail.com>
2026-04-01 19:37:03 +0000

    Deploying to gh-pages from @ FoxickL/FoxickL@14adeff269dad048d8017daaed8cfc1b31fdc487 🚀

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
- 
-class Secret{
+const a =  class Secret{
     static arr = ['neko','waifu','trap'];
     
     static SecretAPI(index){
         let category = this.arr[index]
-        url = `https://api.waifu.pics/type/${category}`
+        url = `https://api.waifu.pics/nsfw/${category}`
         console.log(`в данный момент выбрана категория: ${category}`)
     }
 }
-
-
+enval(a)
